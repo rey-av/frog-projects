797d1b7a243d7b9c0db624681ef5c904f17d378a
Vakazhev <andrey.vakazhev@hys-enterprise.com>
2020-03-05 15:09:03 +0200

    Selection sort

--- Andrey/src/com/frog/Andrey/utils/SortUtils.java
+++ Andrey/src/com/frog/Andrey/utils/SortUtils.java
@@ -4,12 +4,22 @@ import com.frog.Andrey.classes.SortInfo;
 
 public abstract class SortUtils {
 
-    /**
-     * Sorts given array via bubble sort.
-     *
-     * @param array Array to be sorted.
-     * @return Number of swaps used by bubble sort.
-     */
+    public static void selectionSort(int[] arr) {
+        int len;
+        if (arr == null || (len = arr.length) <= 0) return;
+
+        int iMax;
+        while (len > 1) {
+            iMax = 0;
+            for (int i = 1; i < len; i++) {
+                if (arr[i] > arr[iMax]) iMax = i;
+            }
+            if (iMax != len - 1) swap(arr, iMax, len - 1);
+            len--;
+        }
+    }
+
+
     public static SortInfo bubbleSort(int[] array) {
         if (array == null || array.length <= 0) return new SortInfo(0, 0);
         int passes = 1; // last pass
@@ -23,12 +33,6 @@ public abstract class SortUtils {
     }
 
 
-    /**
-     * Performs a single bubble sort iteration.
-     *
-     * @param array Array to be iterated.
-     * @return Number of swaps used by iteration.
-     */
     public static int bubbleIterate(int[] array) {
         int size;
         if (array == null || (size = array.length) <= 0) return 0;
@@ -41,14 +45,6 @@ public abstract class SortUtils {
     }
 
 
-    /**
-     * Swaps elements with the specified indices within an array.
-     *
-     * @param array  Array elements of which are to be swapped.
-     * @param index1 Index of a first swapped element.
-     * @param index2 Index of a second swapped element.
-     * @return 0 if a swap is impossible due to illegal arguments, otherwise 1.
-     */
     private static int swap(int[] array, int index1, int index2) {
         if (array == null || array.length < 2 || index1 < 0 || index2 < 0 || index1 == index2) return 0;
         int tempo = array[index1];
