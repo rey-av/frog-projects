6becc6a3a95c52735e4b60d7d154113bedf44257
Vakazhev <andrey.vakazhev@hys-enterprise.com>
2020-03-05 17:26:40 +0200

    Benchmark added

--- Andrey/src/com/frog/Andrey/sort/BubbleSort.java
+++ Andrey/src/com/frog/Andrey/sort/BubbleSort.java
@@ -10,14 +10,15 @@ import static com.frog.Andrey.utils.Utils.swap;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] ref = {3, 1, 4, 1, 5, 9, 2, 6};
+        final int[] ref = {3, 1, 4, 1, 5, 9, 2, 6};
+        int[] arr;
 
-        // iterate
-        int[] arr = ref.clone();
+        // iterate, show swaps and result checksum
+        arr = ref;
         int swaps = iterate(arr);
         System.out.println(Arrays.toString(arr) + "\n" + swaps + " " + checksum(arr));
 
-        // sort
+        // sort, show passes and swaps
         arr = ref.clone();
         SortInfo info = sort(arr);
         System.out.println(Arrays.toString(arr) + "\n" + info.getPasses() + " " + info.getSwaps());
@@ -25,26 +26,36 @@ public class BubbleSort {
 
 
     public static SortInfo sort(int[] array) {
-        if (array == null || array.length <= 0) return new SortInfo(0, 0);
-        int passes = 1; // last pass
-        int total = 0;
+        if (array == null || array.length <= 0) {
+            return new SortInfo(0, 0);
+        }
+
+        int passes = 1; // count last pass
+        int total = 0; // total swaps
         int swaps;
         while ((swaps = iterate(array)) != 0) {
             total += swaps;
             passes++;
         }
+
         return new SortInfo(passes, total);
     }
 
 
     public static int iterate(int[] array) {
         int size;
-        if (array == null || (size = array.length) <= 0) return 0;
-        int count = 0;
-        for (int index = 1; index < size; index++) {
-            int prev = index - 1;
-            if (array[prev] > array[index]) count += swap(array, prev, index);
+        if (array == null || (size = array.length) <= 0) {
+            return 0;
         }
+
+        int count = 0;
+        int cur; // current
+        int prev; // previous
+        for (cur = 1; cur < size; cur++) {
+            prev = cur - 1;
+            if (array[prev] > array[cur]) count += swap(array, prev, cur);
+        }
+
         return count;
     }
 
