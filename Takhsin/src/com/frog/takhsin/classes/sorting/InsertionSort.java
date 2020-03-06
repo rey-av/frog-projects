b995b66a85c78d7ea588701924c1673e1f4ceac2
Takhsin <takhsin.sarayra@hys-enterprise.com>
2020-03-06 15:27:42 +0200

    Update InsertionSort.java

--- Takhsin/src/com/frog/takhsin/classes/sorting/InsertionSort.java
+++ Takhsin/src/com/frog/takhsin/classes/sorting/InsertionSort.java
@@ -19,31 +19,31 @@ public class InsertionSort extends AbstractSort {
             return arr;
         }
 
-        int[] subArr = Arrays.copyOf(arr, 1); // init subarray
+        int[] sortedArr = Arrays.copyOf(arr, 1); // init subarray
 
         for (int i = 0; i < arr.length - 1; i++) {
             // list logic manually synthesised
             // create new array with size +1 filled with 0
-            subArr = Arrays.copyOf(subArr, i + 2);
+            sortedArr = Arrays.copyOf(sortedArr, i + 2);
             // replace 0 with next number from original array
-            System.arraycopy(arr, i + 1, subArr, subArr.length - 1, 1);
+            System.arraycopy(arr, i + 1, sortedArr, sortedArr.length - 1, 1);
 
-            for (int j = subArr.length - 1; j > 0; j--) {
+            for (int j = sortedArr.length - 1; j > 0; j--) {
                 if (direction.equals(SortDirection.ASC)) {
-                    if (subArr[j] < subArr[j - 1]) {
-                        swap(subArr, j, j - 1);
+                    if (sortedArr[j] < sortedArr[j - 1]) {
+                        swap(sortedArr, j, j - 1);
                         continue;
                     }
                 } else {
-                    if (subArr[j] > subArr[j - 1]) {
-                        swap(subArr, j, j - 1);
+                    if (sortedArr[j] > sortedArr[j - 1]) {
+                        swap(sortedArr, j, j - 1);
                         continue;
                     }
                 }
                 break;
             }
         }
-        return subArr;
+        return sortedArr;
     }
 
 }
