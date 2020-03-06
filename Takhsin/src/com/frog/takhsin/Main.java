867497102d28796b05bb7643d0b18d523d425aa2
Takhsin <takhsin.sarayra@hys-enterprise.com>
2020-03-06 17:44:48 +0200

    Shell sort

--- Takhsin/src/com/frog/takhsin/Main.java
+++ Takhsin/src/com/frog/takhsin/Main.java
@@ -5,6 +5,7 @@ import com.frog.takhsin.classes.sorting.BubbleSort;
 import com.frog.takhsin.classes.sorting.InsertionSort;
 import com.frog.takhsin.classes.sorting.SelectionSort;
 import com.frog.takhsin.classes.sorting.ShakerSort;
+import com.frog.takhsin.classes.sorting.ShellSort;
 import com.frog.takhsin.enums.SortDirection;
 
 import java.util.Arrays;
@@ -14,7 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        List<AbstractSort> sortingAlgorithms = Arrays.asList(new BubbleSort(), new SelectionSort(), new InsertionSort(), new ShakerSort());
+        List<AbstractSort> sortingAlgorithms = Arrays.asList(new BubbleSort(), new SelectionSort(), new InsertionSort(), new ShakerSort(), new ShellSort());
 
         int[] ints = {1, 4, 3, 2, 6, 5, -1};
         for (AbstractSort algorithm : sortingAlgorithms) {
---
+++ Takhsin/src/com/frog/takhsin/classes/sorting/ShellSort.java
@@ -0,0 +1,39 @@
+package com.frog.takhsin.classes.sorting;
+
+import com.frog.takhsin.enums.SortDirection;
+
+public class ShellSort extends AbstractSort {
+
+    public ShellSort() {
+        super("Shell sort");
+    }
+
+
+    @Override
+    public int[] sort(int[] arr, SortDirection direction) {
+        super.sort(arr, direction);
+
+        int gap = arr.length / 2;
+
+        while (gap > 0) {
+            for (int i = 0; i < arr.length - gap; i += gap) {
+                for (int j = 0; j < i + gap && j < arr.length - gap; j++) {
+                    if (direction.equals(SortDirection.ASC)) {
+                        if (arr[j] > arr[j + gap]) {
+                            swap(arr, j, j + gap);
+                        }
+                    } else {
+                        if (arr[j] < arr[j + gap]) {
+                            swap(arr, j, j + gap);
+                        }
+                    }
+                }
+            }
+
+            gap = gap == 2 ? 1 : (int) (gap / 2.2); //The best performance is offered by a sequence by Marcin Ciura
+        }
+
+        return arr;
+    }
+
+}
