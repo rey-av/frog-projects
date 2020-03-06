fae556cf851e9a0ec00c9a698ce289d7028ac493
Nika <nickaromanova1998@gmail.com>
2020-03-06 17:11:59 +0300

    Added bubble, insertion, selection sorts logic.

--- Nika/src/com/frog/nika/sort/sortClass/SortBubbles.java
+++ Nika/src/com/frog/nika/sort/sortClass/SortBubbles.java
@@ -6,7 +6,18 @@ public class SortBubbles extends AbstractSort {
 
     @Override
     public void Sort(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
 
     }
-
 }
--- Nika/src/com/frog/nika/sort/sortClass/SortInsertion.java
+++ Nika/src/com/frog/nika/sort/sortClass/SortInsertion.java
@@ -6,6 +6,17 @@ public class SortInsertion extends AbstractSort {
 
     @Override
     public void Sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+
+            int index = i;
+            int temp = arr[i];
+
+            while (index > 0 && arr[index - 1] >= temp) {
+                arr[index] = arr[index - 1]; // меняем элемент на следующий
+                index--;
+            }
+            arr[index] = temp;
+        }
 
     }
 
