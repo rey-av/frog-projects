a644fa0a20c0c8422d9e98b6bf94a618e970ddab
Ivan! <ivan.yaryga@hys-enterprise.com>
2020-03-10 14:32:41 +0200

    completed rotatestring, added squareroot

---
+++ Ivan/src/com/frog/Ivan/other/RotateString.java
@@ -0,0 +1,27 @@
+package com.frog.Ivan.other;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class RotateString {
+
+    public static void main(String[] args) throws IOException {
+
+        FileReader in = new FileReader("resources/other/rotatestring.txt");
+        BufferedReader br = new BufferedReader(in);
+        String s;
+        while ((s = br.readLine()) != null) {
+            String[] array = s.split(" ");
+            int parseInt = Integer.parseInt(array[0]);
+            if(parseInt < 0 ) {
+                System.out.print(array[1].substring(array[1].length() + parseInt) + array[1].substring(0, array[1].length() + parseInt) + " ");
+            } else if (parseInt > 0) {
+                System.out.print(array[1].substring(parseInt) + array[1].substring(0,parseInt) + " ");
+            } else {
+                System.out.println("Something went wrong ");
+            }
+        }
+    }
+
+}
---
+++ Ivan/src/com/frog/Ivan/other/SquareRoot.java
@@ -0,0 +1,9 @@
+package com.frog.Ivan.other;
+
+//TODO: in progress
+public class SquareRoot {
+
+    public static void main(String[] args) {
+
+    }
+}
