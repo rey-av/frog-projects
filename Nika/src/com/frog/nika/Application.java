912bfcb730270e1a2addc71ccdca350c8d1b3690
Nika <nickaromanova1998@gmail.com>
2020-03-10 09:37:58 +0200

    Added launch sorts methods from main

--- Nika/src/com/frog/nika/Application.java
+++ Nika/src/com/frog/nika/Application.java
@@ -1,14 +1,19 @@
 package com.frog.nika;
 
 import com.frog.nika.search.ManagerSearch;
+import com.frog.nika.sort.EnumSort;
+import com.frog.nika.sort.ManagerSort;
 
 import java.util.Arrays;
+import java.util.SortedMap;
 
 import static com.frog.nika.search.EnumSearch.searcherType;
 
 public class Application {
 
     public static void main(String[] args) {// TODO: добавить логи
+
+        /// -------------------    ПОИСК    ------------------- ///
         ManagerSearch searchManager = new ManagerSearch();
 
         GetData array = new GetData();
@@ -27,7 +32,24 @@ public class Application {
         System.out.println(Arrays.toString(array.getArray()));
         System.out.println("Index of " + searchKey + " : " + resultBinary + "\n");
 
-        int resultError = searchManager.Search(searcherType.ERROR_CHECK, array.getArray(), 100);
+        System.out.println(searchManager.Search(searcherType.ERROR_CHECK, array.getArray(), 100) + "\n");
+
+        /// -------------------    СОРТИРОВКИ    ------------------- ///
+        ManagerSort sortManager = new ManagerSort();
+        int[] arr = array.createRandomArray(10, 1, 100);
+        System.out.println("Unordered array: " + Arrays.toString(arr));
+        sortManager.Sort(EnumSort.sortType.BUBBLE, arr); // упорядочили
+        System.out.println("After BUBBLE SORT: " + Arrays.toString(arr) + "\n");
+
+        arr = array.createRandomArray(10, 1, 100);  // перемешиваем заново
+        System.out.println("Unordered array: " + Arrays.toString(arr));
+        sortManager.Sort(EnumSort.sortType.INSERTION, arr);
+        System.out.println("After INSERTION SORT: " + Arrays.toString(arr) + "\n");
+
+        arr = array.createRandomArray(10, 1, 100);
+        System.out.println("Unordered array: " + Arrays.toString(arr));
+        sortManager.Sort(EnumSort.sortType.SELECTION, arr);
+        System.out.println("After SELECTION SORT: " + Arrays.toString(arr) + "\n");
 
     }
 
