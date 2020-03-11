3468813dc0a0277943a578e6e31b8ec89996128b
Ivan! <ivan.yaryga@hys-enterprise.com>
2020-03-11 16:57:01 +0200

    added bicycle race, finished buuble in array

---
+++ Ivan/src/com/frog/Ivan/other/BicycleRace.java
@@ -0,0 +1,23 @@
+package com.frog.Ivan.other;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class BicycleRace {
+
+    public static void main(String[] args) throws IOException {
+
+        FileReader in = new FileReader("resources/other/bicyclerace.txt");
+        BufferedReader br = new BufferedReader(in);
+        String s;
+        while ((s = br.readLine()) != null) {
+            double[] array = Arrays.stream(s.split(" ")).mapToDouble(Double::parseDouble).toArray();
+            double timeBetween = array[0] / (array[1] + array[2]);
+            double distance = timeBetween * array[1];
+            System.out.print(distance + " ");
+
+        }
+    }
+}
--- Ivan/src/com/frog/Ivan/sort/BuubleInArray.java
+++ Ivan/src/com/frog/Ivan/sort/BuubleInArray.java
@@ -1,5 +1,40 @@
 package com.frog.Ivan.sort;
 
-//TODO: in progress
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class BuubleInArray {
+
+    public static void main(String[] args) throws IOException {
+
+        FileReader in = new FileReader("resources/sort/bubbleinarray.txt");
+        BufferedReader br = new BufferedReader(in);
+        String s;
+        while ((s = br.readLine()) != null) {
+            int[] array = Arrays.stream(s.substring(0, s.length() - 3).split(" ")).mapToInt(Integer::parseInt).toArray();
+            int swaps = 0;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
+                    swaps++;
+                }
+            }
+            long sum = 0;
+            for (int value : array) {
+                sum = (sum + value) * 113;
+                if (sum > 10000007) {
+                    sum = sum % 10000007;
+                }
+            }
+            System.out.println(swaps + " " + sum);
+        }
+    }
+
+    private static void swap(int[] array, int first, int second) {
+        int temp = array[second];
+        array[second] = array[first];
+        array[first] = temp;
+    }
 }
