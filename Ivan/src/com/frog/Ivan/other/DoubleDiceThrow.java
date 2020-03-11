b9f81f7d6b2116a4d8c879c91769859534a9c421
Ivan! <ivan.yaryga@hys-enterprise.com>
2020-03-11 15:29:49 +0200

    completed double dice throw

---
+++ Ivan/src/com/frog/Ivan/other/DoubleDiceThrow.java
@@ -0,0 +1,24 @@
+package com.frog.Ivan.other;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class DoubleDiceThrow {
+
+    public static void main(String[] args) throws IOException {
+
+        FileReader in = new FileReader("resources/other/doubledicethrow.txt");
+        BufferedReader br = new BufferedReader(in);
+        String s;
+        while ((s = br.readLine()) != null) {
+            String[] init = s.split(" ");
+            double[] array = Arrays.stream(init).mapToDouble(Double::parseDouble).toArray();
+            array[0] = Math.round(-(Math.floor(array[0] / 6) - array[0] / 6) * 6) + 1;
+            array[1] = Math.round(-(Math.floor(array[1] / 6) - array[1] / 6) * 6) + 1;
+            int result = (int) (array[0] + array[1]);
+            System.out.print(result + " ");
+        }
+    }
+}
